3bd94f8ba8de5acb549720c4861bf6962c354197
LucaBenedetto25 <luca.benedetto@edu.immaginazioneelavoro.it>
2026-03-04 13:46:15 +0100

    Aggiunta lezione 1

---
+++ Python_Lezioni/Lezione2_(Tipi_di_dato)/0.1_variabli.py
@@ -0,0 +1,23 @@
+# questo è un commento
+#cosa è una variabile?
+# una variabile è un contenitore che memorizza un valore. che può essere di diversi tipi e può cambiare durante l'esecuzione del codice.
+
+#assegno una variabile
+mioNome = "Luca" #stringa
+miaEta = 18 #intero
+corso = "TecnicoInformatico" #stringa
+altezza = 1.74 #float (NUMERO CON LA VIRGOLA)
+
+#indipendentemente dal tipo che scelgo non viene dichiarato da nessuna parte. questo comportamento è tipico dei linguaggi debolmente tipizzati
+
+# ESERCIZIO: Istanzia 2 variabili (nome a scelta libera) di tipo intero. con queste 2 variabili elabora le 4 operazioni matematiche (+ - * /) e stampa il risultato di ogni operazione.
+numero1 = 100
+numero2 = 1.04
+somma = numero1 + numero2
+differenza = numero1 - numero2
+prodotto = numero1 * numero2
+quoziente = numero1 / numero2
+print("La somma è: " + str(somma))
+print("La differenza è: " + str(differenza))
+print("Il prodotto è: " + str(prodotto))
+print("Il quoziente è: " + str(quoziente))
