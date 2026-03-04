649b8967a9744b992f358094e6b7cd6b2363f87c
LucaBenedetto25 <luca.benedetto@edu.immaginazioneelavoro.it>
2026-03-04 14:17:15 +0100

    fine lezione1

--- Python_Lezioni/Lezione2_(Tipi_di_dato)/0.1_variabli.py
+++ Python_Lezioni/Lezione2_(Tipi_di_dato)/0.1_variabli.py
@@ -16,8 +16,31 @@ numero2 = 1.04
 somma = numero1 + numero2
 differenza = numero1 - numero2
 prodotto = numero1 * numero2
-quoziente = numero1 / numero2
+quoziente = (round (numero1 / numero2), 2)
 print("La somma è: " + str(somma))
 print("La differenza è: " + str(differenza))
 print("Il prodotto è: " + str(prodotto))
 print("Il quoziente è: " + str(quoziente))
+
+#numeri con la virgola: float
+pi = 3.14
+temperatura = 36.5
+#voglio controllare di che tipo sono pi e temperatura
+print(type(pi))
+print(type(temperatura)) 
+
+calcolo = 0.1 + 0.2
+print(calcolo) #il risultato è 0.30000000000000004 Attenti alla precisione
+print (round(calcolo, 2)) #dopo la virgola segni quante cifre voglio mostrare
+
+#STRINGHE : sono sequenze immutabili di caratteri. Le stringhe sono il modo umano di comunicare
+
+nomeUser = "Luca"
+emailUser = "alibabba@gmail.com"
+corso = "TecnicoInformatico"
+
+#concateniamo le stringhe
+saluto = "Ciao, mi chiamo " + nomeUser + " e il mio email è " + emailUser + " e frequento il corso " + corso
+print(saluto)
+
+print("ti prego fatemi andare a casa, ho fame, ho sonno, mi sto rompendo le palle, voglio vedere la mia famiglia, voglio vedere i miei amici, voglio vedere il mio cane, voglio vedere il mio gatto, voglio vedere il mio pesce rosso, voglio vedere il mio criceto, voglio vedere il mio coniglio, voglio vedere il mio pappagallo, voglio vedere il mio serpente, voglio vedere il mio camaleonte, voglio vedere il mio ragno, voglio vedere il mio scorpione, voglio vedere il mio tarantola, voglio vedere il mio millepiedi, voglio vedere il mio centopiedi, voglio vedere il mio millepiedi gigante, voglio vedere il mio centopiedi gigante, voglio vedere il mio episodio di Pokémon Accademy per dio")
