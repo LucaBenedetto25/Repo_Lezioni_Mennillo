54fb8678657da93c1f7edf4108bf9c8ac61b89dd
MarsalaAlessandro <alessandro.marsala@edu.immaginazioneelavoro.it>
2026-03-11 13:42:16 +0100

    Esercizi

---
+++ Esercizi/es.1.py
@@ -0,0 +1,14 @@
+numeroA = 15
+numeroB = 4 
+somma = numeroA + numeroB
+differenza = numeroA - numeroB
+prodotto = numeroA * numeroB
+divisione = round (numeroA / numeroB)
+divisionepiùresto = numeroA / numeroB
+potenza = numeroA**numeroB
+print(f"{numeroA} + {numeroB}) fa {somma}")
+print(f"{numeroA} - {numeroB}) fa {differenza}")
+print(f"{numeroA} * {numeroB}) fa {prodotto}")
+print(f"{numeroA} / {numeroB}) fa {divisione}")
+print(f"e se vuoi sapere il resto {numeroA} / {numeroB}) fa {divisionepiùresto}")
+print(f"{numeroA} elevato per {numeroB}) fa {potenza}")
